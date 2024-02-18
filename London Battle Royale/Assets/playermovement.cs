4cb407899e682e936267c5ac865ba9bb0c2e1a7b
Blobiff <ttaldinger@gmail.com>
2024-02-18 22:42:43 +0100

    update

--- London Battle Royale/Assets/playermovement.cs
+++ London Battle Royale/Assets/playermovement.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class playermovement : MonoBehaviour
 {
+    Rigidbody rb;
+    [SerializedField] float MoveSpeed = 10;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float horInput = horInput.GetAxisRaw("Horizontal") * MoveSpeed;
+        float verInput = horInput.GetAxisRaw("Vertical") * MoveSpeed;
+
+        rb.velocity = new Vector3(horInput, 0, verInput)
     }
 }
