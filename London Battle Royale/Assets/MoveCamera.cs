c69107a9822447f8675e5909216d0fe6d6fc5788
Blobiff <ttaldinger@gmail.com>
2024-02-19 19:58:37 +0100

    crazy

--- London Battle Royale/Assets/MoveCamera.cs
+++ London Battle Royale/Assets/MoveCamera.cs
@@ -2,8 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerCam : MonoBehaviour
+public class MoveCamera : MonoBehaviour
 {
+    public Transform cameraPosition;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,6 @@ public class PlayerCam : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position = cameraPosition.position;
     }
 }
